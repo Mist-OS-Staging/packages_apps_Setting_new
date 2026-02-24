xiaomi-sm8450-mondrian/android_packages_apps_Settings

Add support for manually installed google sound picker

src/com/android/settings/RingtonePreference.java
@@ -55,6 +55,7 @@ import androidx.preference.PreferenceManager;
 public class RingtonePreference extends Preference {
 
     private static final String TAG = "RingtonePreference";
+    private static final String GOOGLE_SP_PKG_NAME = "com.google.android.soundpicker";
 
     // Default is slot0
     private int mSlotId = 0;
@@ -80,7 +81,10 @@ public class RingtonePreference extends Preference {
                 true);
         String packageName = context.getString(R.string.config_sound_picker_package_name);
         Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
-        if (com.android.internal.util.android.Utils.isPackageInstalled(context, packageName)) {
+        // prioritize google sound picker if manually installed by user 
+        if (com.android.internal.util.android.Utils.isPackageInstalled(context, GOOGLE_SP_PKG_NAME)) {
+            intent.setPackage(GOOGLE_SP_PKG_NAME);
+        } else if (com.android.internal.util.android.Utils.isPackageInstalled(context, packageName)) {
             intent.setPackage(packageName);
         }
         setIntent(intent);

Mist-OS-Staging/packages_apps_Setting_new

src/com/android/settings/RingtonePreference.java
@@ -54,6 +54,7 @@ import androidx.preference.PreferenceManager;
 public class RingtonePreference extends Preference {
 
     private static final String TAG = "RingtonePreference";
+    private static final String GOOGLE_SP_PKG_NAME = "com.google.android.soundpicker";
 
     private int mRingtoneType;
     private boolean mShowDefault;
@@ -76,7 +77,10 @@ public class RingtonePreference extends Preference {
                 true);
         String packageName = context.getString(R.string.config_sound_picker_package_name);
         Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
-        if (com.android.internal.util.mist.Utils.isPackageInstalled(context, packageName)) {
+        // prioritize google sound picker if manually installed by user
+        if (com.android.internal.util.mist.Utils.isPackageInstalled(context, GOOGLE_SP_PKG_NAME)) {
+            intent.setPackage(GOOGLE_SP_PKG_NAME);
+        } else if (com.android.internal.util.mist.Utils.isPackageInstalled(context, packageName)) {
             intent.setPackage(packageName);
         }
         setIntent(intent);