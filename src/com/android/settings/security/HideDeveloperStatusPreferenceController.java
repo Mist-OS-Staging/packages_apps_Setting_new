hlcynprjct/packages_apps_Settings

Settings: Features: Hide ADB and developer setting enable status [2/2]
* Code is adapted from cutout force full screen
* Give authorship

Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: Tony Mantler <nicoya@google.com>
Co-authored-by: jhenrique09 <jhsv09@gmail.com>
Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Signed-off-by: someone5678 <nemui3353@gmail.com>
Signed-off-by: HeroBuxx <me@herobuxx.me>

src/com/android/settings/security/HideDeveloperStatusPreferenceController.java
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2019 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security;
+
+import android.content.Context;
+import android.content.pm.UserInfo;
+import android.provider.Settings;
+import android.os.UserManager;
+
+import com.android.settings.core.BasePreferenceController;
+
+import com.android.internal.util.halcyon.HideDeveloperStatusUtils;
+
+import java.util.List;
+
+public class HideDeveloperStatusPreferenceController extends BasePreferenceController {
+
+    private static final String PREF_KEY = "hide_developer_status_settings";
+    private static HideDeveloperStatusUtils hideDeveloperStatusUtils = new HideDeveloperStatusUtils();
+
+    private UserManager userManager;
+    private List<UserInfo> userInfos;
+
+    public HideDeveloperStatusPreferenceController(Context context) {
+        super(context, PREF_KEY);
+        userManager = UserManager.get(context);
+        userInfos = userManager.getUsers();
+        for (UserInfo info: userInfos) {
+            hideDeveloperStatusUtils.setApps(context, info.id);
+        }
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+}

Mist-OS-Staging/packages_apps_Setting_new

Change-Id: I622b6e585dbe9e3c219cd4efc69d6fc87ff8a73d

src/com/android/settings/security/HideDeveloperStatusPreferenceController.java
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2019 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security;
+
+import android.content.Context;
+import android.content.pm.UserInfo;
+import android.provider.Settings;
+import android.os.UserManager;
+
+import com.android.settings.core.BasePreferenceController;
+
+import com.android.internal.util.mist.HideDeveloperStatusUtils;
+
+import java.util.List;
+
+public class HideDeveloperStatusPreferenceController extends BasePreferenceController {
+
+    private static final String PREF_KEY = "hide_developer_status_settings";
+    private static HideDeveloperStatusUtils hideDeveloperStatusUtils = new HideDeveloperStatusUtils();
+
+    private UserManager userManager;
+    private List<UserInfo> userInfos;
+
+    public HideDeveloperStatusPreferenceController(Context context) {
+        super(context, PREF_KEY);
+        userManager = UserManager.get(context);
+        userInfos = userManager.getUsers();
+        for (UserInfo info: userInfos) {
+            hideDeveloperStatusUtils.setApps(context, info.id);
+        }
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+}